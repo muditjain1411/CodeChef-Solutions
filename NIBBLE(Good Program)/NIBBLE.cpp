bb0650b668b825e351f979eeec0da95a63334900
Mudit Jain <muditjain.1406@gmail.com>
2026-02-07 19:02:10 +0530

    Added solution for Question Code 'NIBBLE'

---
+++ NIBBLE(Good Program)/NIBBLE.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while(t>0){
+	    
+	    int n;
+	    cin >> n;
+	    cout << ((n%4==0)?"Good":"Not Good") << "\n";
+	    t--;
+	}
+
+}
